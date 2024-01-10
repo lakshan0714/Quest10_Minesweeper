f8505bf775e245f4a117329a02e8282da1acb7c5
Muhthaseem <155508864+Muhthaseem@users.noreply.github.com>
2024-01-10 11:00:44 +0530

    2nd Commit
    Successfully added Functions given to me

--- main.cpp
+++ main.cpp
@@ -34,6 +34,75 @@ Minesweeper::Minesweeper(int size, int minesCount)
     srand(static_cast<unsigned>(time(0)));
 }
 
+void Minesweeper::displayField() {
+    // Display the current state of the field
+    cout << "  ";
+    for (int i = 0; i < gridSize; ++i) {
+        cout << static_cast<char>('A' + i) << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < gridSize; ++i) {
+        cout << static_cast<char>('A' + i) << " ";
+        for (int j = 0; j < gridSize; ++j) {
+            cout << field[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Minesweeper::placeMines() {
+    // Place mines randomly on the field
+    int minesPlaced = 0;
+    while (minesPlaced < numMines) {
+        int row = rand() % gridSize;
+        int col = rand() % gridSize;
+        if (!mines[row][col]) {
+            mines[row][col] = true;
+            ++minesPlaced;
+        }
+    }
+}
+
+void Minesweeper::revealLocation(int row, int col) {
+    // Implement logic to reveal the location and update the field
+    if (mines[row][col]) {
+        cout << "Game Over! You hit a mine." << endl;
+        displayField();
+        exit(0);
+    }
+
+    if (field[row][col] == 'c') {
+        // Reveal the current location
+        int adjacentMines = countAdjacentMines(row, col);
+        if (adjacentMines == 0) {
+            // No mines nearby, reveal neighboring locations recursively
+            field[row][col] = '.';
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    int newRow = row + i;
+                    int newCol = col + j;
+                    if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize) {
+                        revealLocation(newRow, newCol);
+                    }
+                }
+            }
+        } else {
+            // Display the number of adjacent mines
+            field[row][col] = '0' + adjacentMines;
+        }
+    }
+}
+
+void Minesweeper::placeFlag(int row, int col) {
+    // Implement logic to place a flag and update the field
+    if (field[row][col] == 'c') {
+        field[row][col] = 'F';
+        --flagsRemaining;
+    } else {
+        cout << "Invalid move. You can only place a flag on a covered location." << endl;
+    }
+}
 
 
 int main() {
